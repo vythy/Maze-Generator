b860cfdbac70fb5c8fe5c28c53a3031597abdefd
Justin Le <53684485+vythy@users.noreply.github.com>
2021-12-17 09:52:08 -0800

    Update node.cpp

--- src/node.cpp
+++ src/node.cpp
@@ -1,30 +1,45 @@
 #include "../include/node.hpp"
 using namespace std;
 
+// toggles i-th bit from the right
+void toggle_bit(int &target, int i);
+
 // method to destroy top wall
 void Node::carve_top(Node &other) {
     // destroys top wall and the bottom wall of node above it
-    walls ^= (1 << 3);
-    other.walls ^= (1 << 1);
+
+    toggle_bit(walls, 4);
+    toggle_bit(other.walls, 2);
 }   
 
 // method to destroy right wall
 void Node::carve_right(Node &other) {
     // destorys right wall and left wall of node to the right
-    walls ^= (1 << 2);
-    other.walls ^= 1;
+
+    toggle_bit(walls, 3);
+    toggle_bit(other.walls, 1);
 }
 
 // method to destroy bottom wall
 void Node::carve_bottom(Node &other) {
     // destorys bottom wall and top wall of node below
-    walls ^= (1 << 1);
-    other.walls ^= (1 << 3);
+
+    toggle_bit(walls, 2);
+    toggle_bit(other.walls, 4);
 }
 
 // method to destroy left wall
 void Node::carve_left(Node &other) {
     // destorys left wall and right wall of node to the left
-    walls ^= 1;
-    other.walls ^= (1 << 2);
+
+    toggle_bit(walls, 1);
+    toggle_bit(other.walls, 3);
+}
+
+void toggle_bit(int &target, int i) {
+    // select the i-th bit
+    int mask = 1; mask <<= (i-1);
+
+    // xor with mask
+    target ^= mask;
 }
